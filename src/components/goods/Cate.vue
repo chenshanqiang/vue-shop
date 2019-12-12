6ac3fe683545a0162cceea362a6f5b912626e748
chenshanqiang <chenshanqi.ang@163.com>
2019-12-12 17:30:49 +0800

    ''

---
+++ src/components/goods/Cate.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品分类</el-breadcrumb-item>
+    </el-breadcrumb>
+     <!-- 卡片视图区域 -->
+    <el-card>
+      <!-- 添加分类按钮 -->
+      <el-row>
+        <el-col>
+          <el-button type="primary">添加分类</el-button>
+        </el-col>
+      </el-row>
+    </el-card>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="less" scoped>
+  
+</style>
